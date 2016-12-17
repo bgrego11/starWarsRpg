2dbd76dbfd56972aed12202957509b3def4e29da
bgrego11 <natureboy4271@gmail.com>
2016-12-16 21:54:33 -0500

    sounds and battlers done, restyled

--- game.js
+++ game.js
@@ -16,6 +16,9 @@ game = {
 	characters: [ {
 		name: "skywalker",
 		avatar: "http://66.media.tumblr.com/58745472300705702d6b6d3c98098f72/tumblr_inline_nu379kwOEq1sk47ji_100.jpg",
+		battler: "img/lukebattler.png",
+		enterSound: new Audio('audio/lukeEnter.mp3'),
+		atkSound: new Audio('audio/lukeAtk.mp3'),
 		hp: 100,
 		atk: 5,
 		batk: 5,
@@ -24,14 +27,29 @@ game = {
 	{
 		name: "chewbacca",
 		avatar: "http://images6.fanpop.com/image/photos/32800000/Chewbacca-miabear1998-32833718-118-148.jpg",
+		battler: "img/chewBattler.png",
+		enterSound: new Audio('audio/chewEnter.mp3'),
 		hp: 120,
 		atk: 1,
 		batk: 1,
 		ctk: 1
 	},
 	{
+		name: "Han Solo",
+		avatar: "http://images6.fanpop.com/image/photos/36000000/Harrison-Ford-image-harrison-ford-36066130-100-100.png",
+		battler: "img/hanBattler.png",
+		enterSound: new Audio('audio/hanEnter.mp3'),
+		hp: 200,
+		atk: 6,
+		batk: 6,
+		ctk: 12
+	},
+	{
 		name: "Darth Vader",
 		avatar: "http://images5.fanpop.com/image/photos/27400000/Anakin-anakin-skywalker-27449141-100-100.jpg",
+		battler: "img/vaderBattler.png",
+		enterSound: new Audio('audio/vaderEnter.mp3'),
+		atkSound: new Audio('audio/vaderAtk.mp3'),
 		hp: 150,
 		atk: 7,
 		batk: 7,
@@ -65,7 +83,8 @@ for (i in game.characters) {
 $('.charIcon').on("click", function(){
 	if (game.playerChosen === false) {
 	game.player = game.characters[$(this).data('person')];
-	$('#playerBattle').attr('src', game.player.avatar);
+	$('#playerBattle').attr('src', game.player.battler);
+	game.player.enterSound.play();
 	//add name & hp
 	$('#playerName').html('Name: '+game.player.name);
 	$('#playerHp').html('Health: '+game.player.hp);
@@ -74,8 +93,9 @@ $('.charIcon').on("click", function(){
 	$(this).remove();
 } else if (game.aiChosen === false) {
 	game.ai = game.characters[$(this).data('person')];
+	game.ai.enterSound.play();
 	//add pic
-	$('#aiBattle').attr('src', game.ai.avatar);
+	$('#aiBattle').attr('src', game.ai.battler);
 	//add name & hp
 	$('#aiName').html('Name: '+game.ai.name);
 	$('#aiHp').html('Health: '+game.ai.hp);
@@ -91,17 +111,23 @@ $('.charIcon').on("click", function(){
 //game loop should be player select, ai selct, battle until hp = 0 then if enemies remain ai select then battle until hp = 0, else win
 $('#attackBtn').on("click", function() {
 	if (game.battleMode) {
+		$("#aiBattle").fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 		game.battle();
+		game.player.atkSound.play();
 		$('#aiHp').html('Health: '+game.ai.hp);
 		$('#playerHp').html('Health: '+game.player.hp);
 		if (game.player.hp < 0) {
 			console.log("you lost");
 			$('#gameMsg').html("You've Been Defeated");
 		} else if (game.ai.hp < 0) {
+			if (charDiv.html().length > 0) {
 			game.aiChosen = false;
 			game.battleMode = false;
 			$('#gameMsg').html('Choose Your Next Opponent');
-		};
+		} else {
+			$('#gameMsg').html('You Won');
+			game.battleMode = false;
+		}
 	};
-	
+	};
 });
