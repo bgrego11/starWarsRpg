00780b73b51da5e85bdc60c677f64a826b86906c
bgrego11 <natureboy4271@gmail.com>
2016-12-18 16:01:50 -0500

    hp bars

--- game.js
+++ game.js
@@ -20,6 +20,7 @@ game = {
 		enterSound: new Audio('audio/lukeEnter.mp3'),
 		atkSound: new Audio('audio/lukeAtk.mp3'),
 		hp: 100,
+		bhp: 100,
 		atk: 5,
 		batk: 5,
 		ctk: 10
@@ -29,9 +30,11 @@ game = {
 		avatar: "http://images6.fanpop.com/image/photos/32800000/Chewbacca-miabear1998-32833718-118-148.jpg",
 		battler: "img/chewBattler.png",
 		enterSound: new Audio('audio/chewEnter.mp3'),
+		atkSound: new Audio('audio/gunSound.mp3'),
 		hp: 120,
-		atk: 1,
-		batk: 1,
+		bhp: 120,
+		atk: 4,
+		batk: 4,
 		ctk: 1
 	},
 	{
@@ -39,7 +42,9 @@ game = {
 		avatar: "http://images6.fanpop.com/image/photos/36000000/Harrison-Ford-image-harrison-ford-36066130-100-100.png",
 		battler: "img/hanBattler.png",
 		enterSound: new Audio('audio/hanEnter.mp3'),
+		atkSound: new Audio('audio/gunSound.mp3'),
 		hp: 200,
+		bhp: 200,
 		atk: 6,
 		batk: 6,
 		ctk: 12
@@ -51,6 +56,7 @@ game = {
 		enterSound: new Audio('audio/vaderEnter.mp3'),
 		atkSound: new Audio('audio/vaderAtk.mp3'),
 		hp: 150,
+		bhp: 150,
 		atk: 7,
 		batk: 7,
 		ctk: 10
@@ -88,8 +94,9 @@ $('.charIcon').on("click", function(){
 	$('#playerBattle').attr('src', game.player.battler);
 	game.player.enterSound.play();
 	//add name & hp
-	$('#playerName').html('Name: '+game.player.name);
+	$('#playerName').html(game.player.name);
 	$('#playerHp').html('Health: '+game.player.hp);
+	$("#playerBar").css("width", ((game.player.hp/game.player.bhp)*100)+"%");
 	$('#gameMsg').html('Choose Your Opponent');
 	game.playerChosen = true;
 	$(this).remove();
@@ -100,8 +107,9 @@ $('.charIcon').on("click", function(){
 	$('#aiBattle').show();
 	$('#aiBattle').attr('src', game.ai.battler);
 	//add name & hp
-	$('#aiName').html('Name: '+game.ai.name);
+	$('#aiName').html(game.ai.name);
 	$('#aiHp').html('Health: '+game.ai.hp);
+	$("#aiBar").css("width", ((game.ai.hp/game.ai.bhp)*100)+"%");
 	$('#gameMsg').html('Use Attack Command to Fight!');
 	$(this).remove();
 	game.aiChosen = true;
@@ -127,12 +135,15 @@ $('#attackBtn').on("click", function() {
 	if (game.battleMode) {
 		$("#aiBattle").fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 		game.battle();
+		$("#playerBar").css("width", ((game.player.hp/game.player.bhp)*100)+"%");
+		$("#aiBar").css("width", ((game.ai.hp/game.ai.bhp)*100)+"%");
 		game.player.atkSound.play();
 		$('#aiHp').html('Health: '+game.ai.hp);
 		$('#playerHp').html('Health: '+game.player.hp);
 		if (game.player.hp < 0) {
 			console.log("you lost");
 			$('#gameMsg').html("You've Been Defeated");
+			game.battleMode = false;
 		} else if (game.ai.hp < 0) {
 			if (charDiv.html().length > 0) {
 			game.aiChosen = false;
@@ -142,7 +153,7 @@ $('#attackBtn').on("click", function() {
 			$('#gameMsg').html('Choose Your Next Opponent');
 		} else {
 			$('#gameMsg').html('You Won');
-			$("#aiBattle").hide("explode", 1000);
+			$("#aiBattle").hide("explode",{pieces: 16}, 1000);
 			game.battleMode = false;
 		}
 	};
